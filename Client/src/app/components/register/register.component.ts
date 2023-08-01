627b5b90fa2d0a68ebee6e723963f032f9db0d2b
Hachiq <lesiv433@gmail.com>
2023-08-01 10:21:52 +0300

    First user registered successfully

--- Client/src/app/components/register/register.component.ts
+++ Client/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  user = new User();
+
+  constructor(private authService: AuthService) { }
+
+  register(){
+    this.authService.register(this.user).subscribe();
+  }
 
 }
