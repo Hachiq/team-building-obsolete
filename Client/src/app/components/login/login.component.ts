9d74ca7d42d26b58a9950939fe0a60195127fedd
Hachiq <lesiv433@gmail.com>
2023-08-01 17:17:16 +0300

    Add login logic. Retrieving jwt successfully

--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginDto } from 'src/app/models/loginDto';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  user = new LoginDto();
 
+  constructor(private authService: AuthService) { }
+
+  login() {
+    this.authService.login(this.user).subscribe((token: string) => {
+      localStorage.setItem('authToken', token);
+    });
+  }
 }
