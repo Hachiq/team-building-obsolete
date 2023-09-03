6e74a4511d49b45a2c998579d27277ddc4d211f2
Hachiq <lesiv433@gmail.com>
2023-09-03 08:43:59 +0300

    Migrations update. Make fields not nullable

---
+++ Api/Data/AppMigrations/20230903045839_AnnotationsUpdate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.AppMigrations
+{
+    public partial class AnnotationsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Stats_StatId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Stats_StatId",
+                table: "Users",
+                column: "StatId",
+                principalTable: "Stats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Stats_StatId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Stats_StatId",
+                table: "Users",
+                column: "StatId",
+                principalTable: "Stats",
+                principalColumn: "Id");
+        }
+    }
+}
