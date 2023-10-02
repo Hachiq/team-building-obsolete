e86784da840a8859ecdca096e4ee1ccc7176239f
Hachiq <lesiv433@gmail.com>
2023-10-02 20:30:57 +0300

    Log in user once registered

--- Client/src/app/components/register/register.component.ts
+++ Client/src/app/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginDto } from 'src/app/models/loginDto';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-register',
@@ -15,12 +17,33 @@ export class RegisterComponent {
 
   constructor(private authService: AuthService,
     private router: Router,
-    private notificationService: NotificationService) { }
+    private notificationService: NotificationService,
+    private tokenService: TokenService) { }
 
   register(){
     this.authService.register(this.user).subscribe(() => {
       this.notificationService.userRegistered();
-      this.router.navigate(['login']);
+
+      const loginDto: LoginDto = {
+        username: this.user.username,
+        password: this.user.password
+      };
+
+      this.authService.login(loginDto).subscribe((token: string) => {
+        localStorage.setItem('authToken', token);
+        if(this.tokenService.userIsTeamMember()){
+          this.router.navigate(['panel']);
+          return;
+        }
+        this.router.navigate(['home']);
+      }, (error) => {
+        if (error.status === 400){
+          this.errorMessage = error.error;
+        }
+        else {
+          this.errorMessage = 'Undefined error. Please, try again later.'
+        }
+      });
     },
     (error) => {
       if (error.status === 400){
